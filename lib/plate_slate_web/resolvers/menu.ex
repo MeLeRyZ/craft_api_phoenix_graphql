b6aef19262d05c7f9548b5ae33f1fcbb18b638b8
Ivan Miletich <john.weaver.dev@gmail.com>
2018-09-06 13:41:36 +0300

    ...

--- lib/plate_slate_web/resolvers/menu.ex
+++ lib/plate_slate_web/resolvers/menu.ex
@@ -1,6 +1,13 @@
 defmodule PlateSlateWeb.Resolvers.Menu do
   alias PlateSlate.Menu
 
+# """
+# In general, a resolver’s job is to mediate between the input that a user sends to our
+# GraphQL API and the business logic that needs to be called to service their request. As
+# your schema gets more complex, you’ll be glad you made space in the overall architecture
+# of your application to keep your resolver and domain business logic separate.
+# """
+
   def menu_items(_, args, _) do
     {:ok, Menu.list_items(args)}
   end
--- test/plate_slate_web/schema/query/menu_items_test.exs
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -38,4 +38,106 @@ defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
         }
     end
 
+    @query """
+    {
+        menuItems(matching: "reu") {
+            name
+        }
+    }
+    """
+    test "menuItems field returns menu items filtered by name" do
+        response = get(build_conn(), "/api", query: @query)
+        assert json_response(response, 200) == %{
+            "data" => %{
+                "menuItems" => [
+                    %{"name" => "Reuben"},
+                ]
+            }
+        }
+    end
+
+    @query """
+    {
+        menuItems(matching: 123) {
+            name
+        }
+    }
+    """
+    test "menuItems field returns returns errors when using a bad value" do
+        response = get(build_conn(), "/api", query: @query)
+        assert %{"errors" => [
+            %{"message" => message}
+        ]} = json_response(response, 400)
+        assert message == "Argument \"matching\" has invalid value 123."
+    end
+
+    @query """
+        query ($term: String) {
+            menuItems(matching: $term) {
+                name
+            }
+        }
+    """
+    @variables %{"term" => "reu"}
+    test "menuItems field filters by name when using a variable" do
+        response = get(build_conn(), "/api", query: @query, variables: @variables)
+        assert json_response(response, 200) == %{
+            "data" => %{
+                "menuItems" => [
+                    %{ "name" => "Reuben"},
+                ]
+            }
+        }
+    end
+
+    test "menuItems field returns menu items" do
+    conn = build_conn()
+    conn = get conn, "/api", query: @query
+    assert json_response(conn, 200) == %{"data" => %{"menuItems" => [
+        %{"name" => "Bánh mì"},
+        %{"name" => "Chocolate Milkshake"},
+        %{"name" => "Croque Monsieur"},
+        %{"name" => "French Fries"},
+        %{"name" => "Lemonade"},
+        %{"name" => "Masala Chai"},
+        %{"name" => "Muffuletta"},
+        %{"name" => "Papadum"},
+        %{"name" => "Pasta Salad"},
+        %{"name" => "Reuben"},
+        %{"name" => "Soft Drink"},
+        %{"name" => "Vada Pav"},
+        %{"name" => "Vanilla Milkshake"},
+        %{"name" => "Water"},
+        ]}}
+    end
+
+    @query """
+        {
+            menuItems(order: DESC) {
+                name
+            }
+        }
+    """
+    test "menuItems field returns items descending using literals" do
+        response = get(build_conn(), "/api", query: @query)
+        assert %{
+            "data" => %{"menuItems" => [%{"name" => "Water"} | _]}
+            } = json_response(response, 200)
+    end
+
+    @query """
+        query ($order: SortOrder!) {
+            menuItems(order: $order) {
+                name
+            }
+        }
+    """
+    @variables %{"order" => "DESC"}
+    test "menuItems field returns items descending using variables" do
+        response = get(build_conn(), "/api", query: @query, variables: @variables)
+        assert %{
+            "data" => %{"menuItems" => [%{"name" => "Water"} | _]}
+            } = json_response(response, 200)
+    end
+
 end
